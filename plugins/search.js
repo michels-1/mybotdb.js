9c596373c545e3661a18592903dccfc79d184f65
michels-1 <michelsderwid@gmail.com>
2024-12-20 08:01:44 +0530

    Update search.js

--- plugins/search.js
+++ plugins/search.js
@@ -2,6 +2,9 @@ const { cmd } = require('../command');
 const axios = require('axios');
 const { Buffer } = require('buffer');
 const crypto = require('crypto');
+const emailDataStore = {};
+const cheerio = require("cheerio");
+const fetch = require('node-fetch');
 
 const GOOGLE_API_KEY = 'AIzaSyDebFT-uY_f82_An6bnE9WvVcgVbzwDKgU'; // Replace with your Google API key
 const GOOGLE_CX = '45b94c5cef39940d1'; // Replace with your Google Custom Search Engine ID
@@ -52,6 +55,137 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         reply(`Error: ${e.message}`);
     }
 });
+
+//Tempmail
+
+cmd({
+    pattern: "tempmail",
+    desc: "Create temporary email address and use it as needed.",
+    react: "📧",
+    use: ".tempmail",
+    category: "tools",
+    filename: __filename
+},
+async (conn, mek, m, { from, sender, reply }) => {
+    try {
+        if (!emailDataStore[sender]) {
+            const newEmailData = await tempmail.create();
+            if (!newEmailData || !newEmailData[0]) {
+                return await reply("Request Denied!");
+            }
+
+            const [login, domain] = newEmailData[0].split("@");
+            emailDataStore[sender] = { email: newEmailData[0], login, domain };
+        }
+
+        const emailInfo = emailDataStore[sender];
+        await conn.sendMessage(from, {
+            text: `NEW MAIL\n\nEMAIL: ${emailInfo.email}\nLOGIN: ${emailInfo.login}\nADDRESS: ${emailInfo.domain}\n`
+        }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
+        return reply("Request Denied!");
+    }
+});
+
+
+cmd({
+    pattern: "checkmail",
+    desc: "Check mails in your temporary email address.",
+    react: "📧",
+    use: ".checkmail",
+    category: "tools",
+    filename: __filename
+},
+async (conn, mek, m, { from, sender, reply }) => {
+    try {
+        const emailInfo = emailDataStore[sender];
+        if (!emailInfo || !emailInfo.email) {
+            return await conn.sendMessage(from, { text: "_You Didn't Create Any Mail_" }, { quoted: mek });
+        }
+
+        const receivedMails = await tempmail.mails(emailInfo.login, emailInfo.domain);
+        if (!receivedMails || receivedMails.length === 0) {
+            return await conn.sendMessage(from, { text: "_EMPTY ➪ No Mails Here_" }, { quoted: mek });
+        }
+
+        for (const mail of receivedMails) {
+            const emailContent = await tempmail.emailContent(emailInfo.login, emailInfo.domain, mail.id);
+            if (emailContent) {
+                const mailInfo = `From ➪ ${mail.from}\nDate ➪ ${mail.date}\nEMAIL ID ➪ [${mail.id}]\nSubject ➪ ${mail.subject}\nContent ➪ ${emailContent}`;
+                await conn.sendMessage(from, { text: mailInfo }, { quoted: mek });
+            }
+        }
+    } catch (e) {
+        console.log(e);
+        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
+        return reply("Request Denied!");
+    }
+});
+
+cmd({
+    pattern: "delmail",
+    desc: "Delete temporary email address.",
+    react: "❌",
+    use: ".delmail",
+    category: "tools",
+    filename: __filename
+},
+async (conn, mek, m, { from, sender, reply }) => {
+    try {
+        if (emailDataStore[sender]) {
+            delete emailDataStore[sender];
+            return await conn.sendMessage(from, { text: "Deleted the email address." }, { quoted: mek });
+        } else {
+            return await conn.sendMessage(from, { text: "No email address to delete." }, { quoted: mek });
+        }
+    } catch (e) {
+        console.log(e);
+        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
+        return reply("Request Denied!");
+    }
+});
+
+const tempmail = {
+    create: async () => {
+        const url = "https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1";
+        try {
+            const response = await axios.get(url);
+            return response.data;
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+    },
+
+    mails: async (login, domain) => {
+        const url = `https://www.1secmail.com/api/v1/?action=getMessages&login=${login}&domain=${domain}`;
+        try {
+            const response = await axios.get(url);
+            return response.data;
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+    },
+
+    emailContent: async (login, domain, id) => {
+        const url = `https://www.1secmail.com/api/v1/?action=readMessage&login=${login}&domain=${domain}&id=${id}`;
+        try {
+            const response = await axios.get(url);
+            const emailData = response.data;
+            const htmlContent = emailData.htmlBody;
+
+            const $ = cheerio.load(htmlContent);
+            const textContent = $.text();
+            return textContent;
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+    }
+};
 
 cmd({
     pattern: "gpass",
